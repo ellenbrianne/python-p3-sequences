6b9264015ef0a7a106aa2bc463b7171956807152
Ellen Bennett <bennette831@gmail.com>
2023-09-15 19:19:15 -0600

    use slice for provided list to modiy length

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+    if (length == 0):
+        print([])
+    else:
+        print(fibonacci[0:length])
